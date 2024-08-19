e5bd74539d8ee07bb9f6d970eb9ec7053d0b388c
abasitt <abdul.basit@rakuten.com>
2024-08-19 13:08:54 +0000

    updating script

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -5,11 +5,16 @@ CONTEXT=neo
 #install binaries in neo
 echo "install cni binaries..."
 kubectl --context="$CONTEXT" apply -f https://raw.githubusercontent.com/abasitt/kube6/main/calico/ipv6_multus/kind/cni-manifest.yaml
+kubectl --context="$CONTEXT" apply -f https://raw.githubusercontent.com/redhat-nfvpe/cni-route-override/master/deployments/daemonset-install.yaml
 
 # Wait for all DaemonSet pods to be in the Running state
 echo "Waiting for DaemonSet pods to be ready..."
 kubectl --context="$CONTEXT" rollout status ds/install-cni-plugins -n kube-system
+kubectl --context="$CONTEXT" rollout status ds/route-override -n kube-system
+
+sleep 10
 
 # Delete the DaemonSet after all pods are running
 echo "Deleting the DaemonSet..."
 kubectl --context="$CONTEXT" delete -f https://raw.githubusercontent.com/abasitt/kube6/main/calico/ipv6_multus/kind/cni-manifest.yaml
+kubectl --context="$CONTEXT" delete -f https://raw.githubusercontent.com/redhat-nfvpe/cni-route-override/master/deployments/daemonset-install.yaml
